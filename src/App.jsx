157e4b962fe7f537be360d0030f15e4bb909a685
Amit ASJ <amitsisodiyajadaun@gmail.com>
2024-01-01 21:58:25 +0530

    source update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import react from "react";
+
+import LudoBoard from "./components/LudoBoard";
+import DiceBoard from "./components/DiceBoard";
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-row justify-between gap-9">
+      <LudoBoard />
+      <DiceBoard />
+    </div>
+  );
 }
 
 export default App;
